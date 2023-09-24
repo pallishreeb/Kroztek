b0e44ea5b59ee22d5fb1fd74f6552f5ff44840d8
Pallishree <pallishreebehera01@gmail.com>
2023-09-25 00:39:38 +0530

    ip update

--- kroztek-client/src/config.js
+++ kroztek-client/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://16.170.230.19:8000/dev/api/v1'
-export const APP_URL = "http://16.170.230.19";
+export const API_URL = 'http://51.20.44.126:8000/dev/api/v1'
+export const APP_URL = "http://51.20.44.126";
 //export const API_URL = 'https://expensive-mite-underclothes.cyclic.app/dev/api/v1'
 // export const APP_URL = "http://localhost:3000";
