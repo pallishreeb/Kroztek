a108b25a3632ff8f629d295512a14ebc5b098e6c
Pallishree <pallishreebehera01@gmail.com>
2024-08-04 17:16:57 +0530

    new build

--- kroztek-api/app.js
+++ kroztek-api/app.js
@@ -39,7 +39,7 @@ require("./config/passport")(passport);
 
 //connect to DB
 connectDb()
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/dev/api/uploads', express.static(path.join(__dirname, 'uploads')));
 app.get("/", (_,res) =>{
     res.send("Welcome to Kroztek integrated solution!")
 })
