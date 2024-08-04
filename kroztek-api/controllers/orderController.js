e0889641cc5a039a55c4eb1fa8fe00e1af267ebc
Pallishree <pallishreebehera01@gmail.com>
2024-08-04 23:10:08 +0530

    orders for admin

--- kroztek-api/controllers/orderController.js
+++ kroztek-api/controllers/orderController.js
@@ -49,3 +49,53 @@ exports.getOrdersByUser = async (req, res) => {
   const orders = await Order.find({ user: userId }).populate('products.product');
   res.status(200).json(orders);
 };
+
+
+// Get all orders
+exports.getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find().populate('products.product');
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching orders', error });
+  }
+};
+
+// Update an order
+exports.updateOrder = async (req, res) => {
+  const { orderId } = req.params;
+  const { paymentStatus, status } = req.body;
+
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      orderId,
+      { paymentStatus, status },
+      { new: true }
+    ).populate('products.product');
+
+    if (!updatedOrder) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+
+    res.status(200).json(updatedOrder);
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating order', error });
+  }
+};
+
+// Delete an order
+exports.deleteOrder = async (req, res) => {
+  const { orderId } = req.params;
+
+  try {
+    const deletedOrder = await Order.findByIdAndDelete(orderId);
+
+    if (!deletedOrder) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+
+    res.status(200).json({ message: 'Order deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting order', error });
+  }
+};
--- kroztek-api/routes/orderRoutes.js
+++ kroztek-api/routes/orderRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
-
+const passport = require("passport");
 router.post('/create', orderController.createOrder);
+
+// Get all orders
+router.get('/',  passport.authenticate("jwt", { session: false }),orderController.getAllOrders);
+
+// Update an order
+router.put('/:orderId', passport.authenticate("jwt", { session: false }), orderController.updateOrder);
+
+// Delete an order
+router.delete('/:orderId',  passport.authenticate("jwt", { session: false }),orderController.deleteOrder);
+
 router.get('/:orderId', orderController.getOrder);
 router.get('/user/:userId', orderController.getOrdersByUser);
-
 module.exports = router;
